2a2f80f5daa7c7cf374bee53c579fcb4def32be4
Alberto Rodriguez <alrodriguezgar14@gmail.com>
2024-02-01 22:18:53 +0100

    created macro-struct. Initiated separately the window and the content

--- main.c
+++ main.c
@@ -12,27 +12,32 @@
 
 #include "fractol.h"
 
-typedef struct s_img
+typedef struct s_data
 {
+	void	*mlx;
+	void	*win;
     void    *img;
     char    *addr;
     int     bits_per_pixel;
     int     line_length;
     int     endian;
-}   t_img;
-
-typedef struct s_data
-{
-	void	*mlx;
-	void	*win;
 	int		color_base;
-	int		zoom;
 	char	*name;
-    t_img 	*img;
+	int		x;
+	int		y;
+	double	offset_x;
+	double	offset_y;
+	double	zoom;
+	double	zx;
+	double	zy;
+	double	cx;
+	double	cy;
+	int		max_iterations;
+
 }	t_data;
 
 
-void    my_mlx_pixel_put(t_img *img, int x, int y, int color)
+void    my_mlx_pixel_put(t_data *img, int x, int y, int color)
 {
     char    *dst;
     int     offset;
@@ -52,24 +57,29 @@ void	print_box(t_data *data, int initial_y, int initial_x, int color_add)
         jdx = initial_x;
         while (jdx++ < initial_x + 25 && jdx < WIDTH - 1)
         {
-            my_mlx_pixel_put(data->img, jdx, idx, data->color_base * color_add);
+            my_mlx_pixel_put(data, jdx, idx, data->color_base * color_add);
         }
     }
-    mlx_put_image_to_window(data->mlx, data->win, data->img->img, 0, 0);
+    mlx_put_image_to_window(data->mlx, data->win, data->img, 0, 0);
 }
 
-void	data_init(t_data *data)
+void	init_mlx(t_data *data)
 {
-	t_img	*img;
-
-	img = malloc(sizeof(t_img));
 	data->mlx = mlx_init();
 	data->win = mlx_new_window(data->mlx, WIDTH, HEIGHT, data->name);
-    img->img = mlx_new_image(data->mlx, WIDTH, HEIGHT);
-    img->addr = mlx_get_data_addr(img->img, &img->bits_per_pixel, &img->line_length, &img->endian);
-	data->img = img;
+    data->img = mlx_new_image(data->mlx, WIDTH, HEIGHT);
+    data->addr = mlx_get_data_addr(data->img, &data->bits_per_pixel, &data->line_length, &data->endian);
+}
+void	init_fractal(t_data *data)
+{
+
 	data->color_base = 0x99bbff;
 	data->zoom = 10;
+	data->x = 0;
+	data->y = 0;
+	data->offset_x = -1.21;
+	data->offset_y = -1.21;
+	data->max_iterations = 50;
 }
 
 void	fill_with_blocks(t_data *fractal, int size)
@@ -121,8 +131,9 @@ int	main(int argc, char **argv)
 	fractal = (t_data *)malloc(sizeof(t_data));
 	if (!fractal)
 		return -1;
+	init_fractal(fractal);
 	fractal->name = ft_sprintf("%s", argv[1]);
-	data_init(fractal);
+	init_mlx(fractal);
 	fill_with_blocks(fractal, 50);
 	mlx_key_hook(fractal->win, key_hook, fractal);
 	mlx_hook(fractal->win, 17, 0, close_window, fractal);
